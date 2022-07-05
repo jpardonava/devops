7b44119e9e808cf10a6a252b3ca6c0b674a4aa6e
localuser <localuser@vmgestion.example.domain>
2022-07-05 23:23:22 +0200

    - configuracion con ansible-galaxy y avanzando en kubernetes

---
+++ ansible2/desploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Ejecutamos el playbook de despliegue de Kubernetes
+ansible-playbook -i ./hosts despliegue-kubernetes.yml
+
+# Ejecutamos el playbook de despliegue de la aplicación
+#ansible-playbook -i ./hosts despliegue-aplicacion.yml
